06769a3831efd83b8125b2df989484937cca7355
Czar102 <lo2.galinskic@gmail.com>
2024-11-03 19:32:40 +0100

    fix solidity version

--- src/BlueprintManager.sol
+++ src/BlueprintManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity 0.8.27;
 
 import { HashLib } from "./libraries/HashLib.sol";
 import { IBlueprint } from "./interfaces/IBlueprint.sol";
