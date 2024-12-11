0ea3573c663c37989f89f7c8a60ca6f14a39c606
grilucosy <grilucosy@gmail.com>
2024-12-10 17:04:49 -0800

    refactore: change ERC1155Wrapper test file to accomodate existing test format

--- test/ERC1155Wrapper.t.sol
+++ test/ERC1155Wrapper.t.sol
@@ -18,58 +18,48 @@ contract ERC1155Wrapper_Gas is Test {
 		vm.deal(address(this), type(uint).max);
 	}
 
-	function test_arrayDecodeWithAssemblyAndCallData() public {
+	function test_arrayDecodeWithAssemblyAndCallData(        
+		address erc1155,
+		address to,
+		uint256[] memory ids,
+		uint256[] memory amounts,
+        bytes memory data
+    ) public {
+        bytes memory encodedData = abi.encode(erc1155, to, ids, amounts, data);
 		(
-            address erc1155,
-            address to,
-            uint256[] memory ids,
-            uint256[] memory amounts,
-            bytes memory data
-	    ) = erc1155wrapper.executeActionNew(_getEncodedActionData());
+            address erc1155New,
+            address toNew,
+            uint256[] memory idsNew,
+            uint256[] memory amountsNew,
+            bytes memory dataNew
+	    ) = erc1155wrapper.executeActionNew(encodedData);
 		(
             address erc1155Old,
             address toOld,
             uint256[] memory idsOld,
             uint256[] memory amountsOld,
             bytes memory dataOld
-	    ) = erc1155wrapper.executeActionOld(_getEncodedActionData());
+	    ) = erc1155wrapper.executeActionOld(encodedData);
 		assertEq(erc1155, erc1155Old);
 		assertEq(to, toOld);
 		assertEq(ids, idsOld);
 		assertEq(amounts, amountsOld);
 		assertEq(data, dataOld);
+		assertEq(erc1155, erc1155New);
+		assertEq(to, toNew);
+		assertEq(ids, idsNew);
+		assertEq(amounts, amountsNew);
+		assertEq(data, dataNew);
 	}
     
-    function _getEncodedActionData() internal pure returns(bytes memory){
-        address erc1155 = 0xe6dAed993cFC56aaC97c9a19DE7E9f8c00e46208;
-        address to = 0xe6dAed993cFC56aaC97c9a19DE7E9f8c00e46208;
-        uint256[] memory ids = new uint256[](10);
-        uint256[] memory amounts = new uint256[](10);
-        bytes memory data = "abc";
-
-        ids[0] = 1;
-        ids[1] = 2;
-        ids[2] = 3;
-        ids[3] = 4;
-        ids[4] = 5;
-        ids[5] = 6;
-        ids[6] = 7;
-        ids[7] = 8;
-        ids[8] = 9;
-        ids[9] = 10;
-
-        amounts[0] = 100;
-        amounts[1] = 200;
-        amounts[2] = 300;
-        amounts[3] = 400;
-        amounts[4] = 500;
-        amounts[5] = 600;
-        amounts[6] = 700;
-        amounts[7] = 800;
-        amounts[8] = 900;
-        amounts[9] = 1000;
-
-        bytes memory encodedData = abi.encode(erc1155, to, ids, amounts, data);
-        return encodedData;
-    }
+    // function _getEncodedActionData(
+	// 	address erc1155,
+	// 	uint256 to,
+	// 	uint256[] memory ids,
+	// 	uint256[] memory amounts,
+    //     bytes memory data
+    // ) internal pure returns(bytes memory) {
+    //     bytes memory encodedData = abi.encode(erc1155, to, ids, amounts, data);
+    //     return encodedData;
+    // }
 }
