57c1861d9e18e7708ca95e8fdd261b575073c2ec
Czar102 <lo2.galinskic@gmail.com>
2024-12-26 14:04:34 +0100

    refactor id derivation logic

--- src/blueprints/options/draft-SimpleOptionBlueprint.sol
+++ src/blueprints/options/draft-SimpleOptionBlueprint.sol
@@ -5,7 +5,7 @@ import {BasicBlueprint, TokenOp, IBlueprintManager} from "../BasicBlueprint.sol"
 import {gcd} from "../../libraries/Math.sol";
 
 contract SimpleOptionBlueprint is BasicBlueprint {
-	mapping (uint256 baseId => uint256 count) reserves;
+	mapping (uint256 baseId => uint256 count) public reserves;
 
 	constructor(IBlueprintManager manager) BasicBlueprint(manager) {}
 
@@ -31,33 +31,17 @@ contract SimpleOptionBlueprint is BasicBlueprint {
 
 		TokenOp[] memory giveTake = oneOperationArray(token0, num);
 
-		uint256 count = gcd(num, denom);
-		(num, denom) = (num / count, denom / count);
-
-		bool swap;
-		if (swap = token1 > token0) {
-			(token0, token1) = (token1, token0);
-			(num, denom) = (denom, num);
-		}
-
-		uint256 baseId = uint256(keccak256(
-			abi.encodePacked(token0, token1, num, denom, expiry, settlement, settler)
-		));
+		(uint256 short, uint256 long, uint256 amount) =
+			getTokens(token0, token1, num, denom, expiry, settlement, settler);
 
 		TokenOp[] memory mintBurn = new TokenOp[](2);
-		unchecked {
-			// short option
-			mintBurn[0] = TokenOp(baseId + 2, count);
-			// now the baseId will be the call/put
-			if (swap)
-				baseId++;
-			mintBurn[1] = TokenOp(baseId, count);
-		}
+		mintBurn[0] = TokenOp(short, amount);
+		mintBurn[1] = TokenOp(long, amount);
 
 		if (mint)
-			reserves[baseId] += count;
+			reserves[long] += amount;
 		else // underflow check prevents from going beyond reserves
-			reserves[baseId] -= count;
+			reserves[long] -= amount;
 
 		return mint ?
 			(mintBurn, zero(), zero(), giveTake) :
@@ -72,26 +56,42 @@ contract SimpleOptionBlueprint is BasicBlueprint {
 		uint256 denom,
 		uint256 expiry,
 		uint256 settlement,
-		address settler,
-		uint256 amount
+		address settler
 	) external {
 		if (block.timestamp <= expiry || (block.timestamp <= settlement && msg.sender != settler))
 			revert AccessDenied();
 
-		bool swap;
-		if (swap = token1 > token0) {
+		(,uint256 long, uint256 amount) =
+			getTokens(token0, token1, num, denom, expiry, settlement, settler);
+
+		blueprintManager.mint(to, long, amount);
+	}
+
+	function getTokens(
+		uint256 token0,
+		uint256 token1,
+		uint256 num,
+		uint256 denom,
+		uint256 expiry,
+		uint256 settlement,
+		address settler
+	) internal pure returns (uint256 short, uint256 long, uint256 amount) {
+		amount = gcd(num, denom);
+		(num, denom) = (num / amount, denom / amount);
+
+		bool swap = token0 < token1;
+		if (swap) {
 			(token0, token1) = (token1, token0);
 			(num, denom) = (denom, num);
 		}
 
+		uint256 id = uint256(keccak256(
+			abi.encodePacked(token0, token1, num, denom, expiry, settlement, settler)
+		));
+
 		unchecked {
-			blueprintManager.mint(
-				to,
-				uint256(keccak256(
-					abi.encodePacked(token0, token1, num, denom, expiry, settlement, settler)
-				)) + (swap ? 1 : 0),
-				amount
-			);
+			short = id + 2;
+			long = id + (swap ? 1 : 0);
 		}
 	}
 }
